8878cddde9432ed497a15df0d0ee540e7d72b2db
Becca <becca.the.tech@gmail.com>
2022-04-30 13:46:21 -0400

    Created a form with basic styling as basis for my bullet journal input

---
+++ day1.js
@@ -0,0 +1,6 @@
+const date = document.querySelector("#date");
+let time = document.querySelector("#time");
+let activityName = document.querySelector("#activityName");
+let events = document.querySelector("#events");
+let notes = document.querySelector("#notes");
+
